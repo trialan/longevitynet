1abca7593c39497445e70eb8af09be08bffe9408
Thomas Rialan <thomasrialan@gmail.com>
2023-10-16 15:48:38 +0200

    eval scripts: data ablation

--- modelling/evaluation/benchmarking.py
+++ modelling/evaluation/benchmarking.py
@@ -1,9 +1,9 @@
 from torch.nn import MSELoss
-from prettytable import PrettyTable
 import glob
 import numpy as np
 import torch
 from tqdm import tqdm
+from prettytable import PrettyTable
 
 from life_expectancy.modelling.data import generate_dataset, _get_train_test_split
 from life_expectancy.modelling.model import ResNet50, FaceAgeDataset
@@ -13,55 +13,70 @@ from life_expectancy.modelling.utils import set_seed
 device = torch.device("mps")
 MODEL = "/Users/thomasrialan/Documents/code/longevity_project/saved_model_binaries/best_model_20231013-145803_0p01107617188245058.pth"
 
-def main():
-    set_seed(SEED)
 
-    _, test_dataloader = get_dataloaders(DS_VERSION, BATCH_SIZE, SEED)
+def get_data():
+    test_dataloader = get_dataloaders(DS_VERSION, BATCH_SIZE, SEED)[1]
     dataset = generate_dataset(DS_VERSION)
+    return test_dataloader, dataset
 
-    test_targets = []
-    for _, _, _, target in tqdm(test_dataloader, desc="Loading targets"):
-        test_targets.extend(target.cpu().numpy())
 
-    # Compute batch-wise MSELoss for the model and then average
-    cumulative_mse_loss = 0.0
+def compute_batchwise_mse(predictions, targets, test_dataloader):
+    cumulative_loss = 0.0
+    for i, (_, _, _, batch_targets) in enumerate(test_dataloader):
+        cumulative_loss += MSELoss()(torch.tensor(predictions[i * BATCH_SIZE: (i + 1) * BATCH_SIZE], dtype=torch.float32).to(device), batch_targets.to(device)).item()
+    return cumulative_loss / len(test_dataloader)
+
+
+def get_model_mse(test_dataloader):
     model = ResNet50()
     path = max(glob.glob(MODEL))
     model.load_state_dict(torch.load(path, map_location=device))
     model = model.to(device)
     model.eval()
-    with torch.no_grad():
-        for imgs, _, _, targets in tqdm(test_dataloader, desc="Computing preds"):
-            imgs = imgs.to(device)
-            outputs = model(imgs)
-            cumulative_mse_loss += MSELoss()(outputs, targets.to(device)).item()
-    mse_loss = cumulative_mse_loss / len(test_dataloader)
 
-    # Baseline Predictions
+    predictions = []
+    with torch.no_grad():
+        for imgs, _, _, _ in tqdm(test_dataloader, desc="Computing preds"):
+            imgs = imgs.to(device)
+            output = model(imgs)
+            predictions.extend(output.cpu().numpy())
+
+    return compute_batchwise_mse(predictions, None, test_dataloader)
+
+
+def get_mean_guess_mse(dataset, test_dataloader):
     train_targets = [row[3].item() for row in dataset]
     train_mean_life_expect = np.mean(train_targets)
+    mean_guesses = [train_mean_life_expect] * len(test_dataloader.dataset)
+    return compute_batchwise_mse(mean_guesses, None, test_dataloader)
 
-    # Compute batch-wise MSELoss for mean_guess and then average
-    cumulative_mean_guess_loss = 0.0
-    mean_guesses = [train_mean_life_expect] * BATCH_SIZE
-    for _, _, _, targets in tqdm(test_dataloader, desc="Mean guess"):
-        cumulative_mean_guess_loss += MSELoss()(torch.tensor(mean_guesses[:len(targets)], dtype=torch.float32).to(device), targets.to(device)).item()
-    mean_guess_mse = cumulative_mean_guess_loss / len(test_dataloader)
 
-    # Compute batch-wise MSELoss for random_guess and then average
+def get_random_guess_mse(dataset, test_dataloader):
+    train_targets = [row[3].item() for row in dataset]
     min_life_expect = np.min(train_targets)
     max_life_expect = np.max(train_targets)
-    cumulative_random_guess_loss = 0.0
-    for _, _, _, targets in tqdm(test_dataloader, desc="Random guess"):
-        random_guesses_batch = np.random.uniform(min_life_expect, max_life_expect, len(targets))
-        cumulative_random_guess_loss += MSELoss()(torch.tensor(random_guesses_batch, dtype=torch.float32).to(device), targets.to(device)).item()
-    random_guess_mse = cumulative_random_guess_loss / len(test_dataloader)
+    random_guesses = np.random.uniform(min_life_expect, max_life_expect, len(test_dataloader.dataset))
+    return compute_batchwise_mse(random_guesses, None, test_dataloader)
 
+
+def create_results_table(model_mse, mean_guess_mse, random_guess_mse):
     table = PrettyTable()
     table.field_names = ["Method", "MSE"]
-    table.add_row(["Model", f"{mse_loss:.6f}"])
+    table.add_row(["Model", f"{model_mse:.6f}"])
     table.add_row(["Mean Guess", f"{mean_guess_mse:.6f}"])
     table.add_row(["Random Guess", f"{random_guess_mse:.6f}"])
+    return table
+
+
+def main():
+    set_seed(SEED)
+    test_dataloader, dataset = get_data()
+
+    model_mse = get_model_mse(test_dataloader)
+    mean_guess_mse = get_mean_guess_mse(dataset, test_dataloader)
+    random_guess_mse = get_random_guess_mse(dataset, test_dataloader)
+
+    table = create_results_table(model_mse, mean_guess_mse, random_guess_mse)
     print(table)
 
 
---
+++ modelling/evaluation/data_ablation.py
@@ -0,0 +1,68 @@
+from tqdm import tqdm
+import datetime
+import torch
+import matplotlib.pyplot as plt
+import random
+from life_expectancy.modelling.model import ResNet50
+from life_expectancy.modelling.data import get_dataloaders
+from life_expectancy.modelling.utils import set_seed, save_model
+import warnings
+warnings.simplefilter("ignore")
+
+device = torch.device("mps")
+N_EPOCHS = 3
+SEED = 7457769
+LR = 1e-3 / 3
+BATCH_SIZE = 128
+DS_VERSION = "v3"
+
+
+if __name__ == '__main__':
+    set_seed(SEED)
+    fractions = [1.0, 0.65, 0.35]
+    results = {}
+    for fraction in fractions:
+        train_dataloader, test_dataloader = get_dataloaders(DS_VERSION,
+                                                            BATCH_SIZE,
+                                                            SEED)
+        N = int(fraction * len(train_dataloader))
+        print(f" *** Fraction is {fraction}, {N} batches *** ")
+
+        model = ResNet50().to(device)
+        model.train()
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+        best_test_loss = float('inf')
+        for epoch in range(N_EPOCHS):
+            start_time = datetime.datetime.now()
+            print(f"Starting epoch {epoch + 1} at", start_time)
+            for i, (imgs, _, _, target) in enumerate(tqdm(train_dataloader)):
+                if i >= N:#int(0.05*len(train_dataloader)):
+                    break
+                imgs = imgs.to(device)
+                target = target.to(device)
+                optimizer.zero_grad()
+                output = model(imgs)
+                loss = criterion(output, target)
+                loss.backward()
+                optimizer.step()
+            end_time = datetime.datetime.now()
+            print(f"Finished epoch {epoch + 1} at", end_time, "Duration:", end_time - start_time)
+
+        model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for imgs, _, _, target in tqdm(test_dataloader):
+                imgs = imgs.to(device)
+                target = target.to(device)
+                output = model(imgs)
+                loss = criterion(output, target)
+                test_loss += loss.item()
+        test_loss = test_loss / len(test_dataloader)
+
+        print("=======================================")
+
+        results[fraction] = test_loss
+
+    for fraction, loss in results.items():
+        print(f"Fraction: {fraction * 100:.0f}%, Test Loss: {loss:.4f}")
