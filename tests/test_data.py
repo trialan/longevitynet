7713b7a09c3010ff454f53daa9be412ce5dc3881
Thomas Rialan <thomasrialan@gmail.com>
2023-10-27 11:41:38 +0200

    TDD

---
+++ tests/test_data.py
@@ -0,0 +1,42 @@
+import pytest
+
+from life_expectancy.modelling.config import CONFIG
+from life_expectancy.modelling.data import (get_train_image_paths,
+                                            get_val_test_image_paths,
+                                            get_file_data)
+
+
+def test_no_overlap_between_train_and_test():
+    """ Don't test overlap in files, test overlap in people names """
+    ds_version = CONFIG["DS_VERSION"]
+    train_image_paths = get_train_image_paths(ds_version)
+    test_image_paths = get_val_test_image_paths()
+    people_in_train = [get_file_data(f)['person_name'] for f
+                       in train_image_paths]
+    people_in_test = [get_file_data(f)['person_name'] for f
+                       in test_image_paths]
+    overlap = set(people_in_train).intersection(people_in_test)
+    assert len(overlap) == 0
+
+
+def test_getting_file_data():
+    example_paths = get_val_test_image_paths()[:4]
+    expected_data = [{"person_name": "Carsten Eggers",
+                      "birth": 1957, "death": 2021,
+                      "img_date": 2000, "life_expectancy": 21},
+                     {"person_name": "Béla Éless",
+                      "birth": 1940, "death": 2020,
+                      "img_date": 2014, "life_expectancy": 6},
+                     {"person_name": "Claude Gingras",
+                      "birth": 1931, "death": 2018,
+                      "img_date": 2017, "life_expectancy": 1},
+                     {"person_name": "Franz Muheim",
+                      "birth": 1931, "death": 2020,
+                      "img_date": 1982, "life_expectancy": 32}]
+
+    for i, path in enumerate(example_paths):
+        data = get_file_data(path)
+        assert data['person_name'] == expected_data[i]['person_name']
+        assert data['death'] == expected_data[i]['death']
+        assert data['birth'] == expected_data[i]['birth']
+
---
+++ tests/test_evaluation.py
@@ -0,0 +1,83 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from life_expectancy.modelling.utils import min_max_scale
+from life_expectancy.modelling.config import CONFIG
+from life_expectancy.modelling.data import (get_val_test_datasets,
+                                            get_train_dataset)
+from life_expectancy.analysis.eval import (get_mean_guess_stats,
+                                           get_model_stats)
+from life_expectancy.modelling.model import ResNet
+
+CONFIG["BATCH_SIZE"] = 1
+expected_train_targets = np.array([0.13235294, 0.22058824, 1., 0.,
+                                   0.01470588,  0.79411765])
+expected_val_targets = np.array([0., 1., 0.27027027, 0.16216216,
+                                 0.45945946, 0.27027027])
+
+
+class MeanPredictingModel(ResNet):
+    def forward(self, img, age):
+        mean = np.mean(expected_train_targets)
+        return torch.Tensor([mean]).unsqueeze(-1)
+
+
+def test_mean_guess_stats():
+    dummy_train_ds = get_dummy_train_ds()
+    dummy_val_ds = get_dummy_val_ds()
+    dummy_val_dl = get_dummy_val_dl()
+    dataset_dict = {"datasets": {"train": dummy_train_ds,
+                                 "validation": dummy_val_ds},
+                    "dataloaders": {"validation": dummy_val_dl}}
+
+    expected_mae = get_expected_mae(dummy_train_ds,
+                                    dummy_val_ds)
+    mae = get_mean_guess_stats(dataset_dict)["mae"]
+    assert np.isclose(mae, expected_mae, atol=1e-5)
+
+    model = MeanPredictingModel()
+    model_mae = get_model_stats(model, dataset_dict)["mae"]
+    assert np.isclose(model_mae, expected_mae, atol=1e-5)
+
+
+def get_expected_mae(dummy_train_ds, dummy_val_ds):
+    train_targets = dummy_train_ds.targets
+    val_targets = dummy_val_ds.targets
+    mean_target = 0.3602941183333333
+    expected_mae = 0.24622416557057059 # mean abs. error on the val
+    scaling_factor = 37 #max - min of the DELTAS
+    expected_mae = expected_mae * scaling_factor
+    return expected_mae
+
+
+def get_dummy_val_dl():
+    dummy_val_ds = get_dummy_val_ds()
+    dl = DataLoader(dummy_val_ds, batch_size=1, shuffle=False)
+    return dl
+
+
+def get_dummy_train_dl():
+    dummy_val_ds = get_dummy_train_ds()
+    dl = DataLoader(dummy_val_ds, batch_size=1, shuffle=False)
+    return dl
+
+
+def get_dummy_val_ds():
+    CONFIG["DATA_FRACTION"] = 1/500
+    dummy_val_ds, _ = get_val_test_datasets(CONFIG)
+    assert all(np.isclose(dummy_val_ds.targets,
+                      expected_val_targets,
+                      atol=0.00001))
+    return dummy_val_ds
+
+
+def get_dummy_train_ds():
+    CONFIG["DATA_FRACTION"] = 1/(500 * 5)
+    dummy_train_ds = get_train_dataset(CONFIG)
+    assert all(np.isclose(dummy_train_ds.targets,
+                      expected_train_targets,
+                      atol=0.00001))
+    return dummy_train_ds
+
+
