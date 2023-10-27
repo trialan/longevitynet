104dd5e4699078eaf2e6ba14b391a905d77274a0
Thomas Rialan <thomasrialan@gmail.com>
2023-10-27 13:26:45 +0200

    b

--- modelling/config.py
+++ modelling/config.py
@@ -4,14 +4,13 @@ from pprint import pprint
 from life_expectancy.modelling.utils import min_max_scale, whiten
 from life_expectancy import get_repo_dir
 
-
 REPO_DIR = get_repo_dir()
 
 CONFIG = {"N_EPOCHS" : 25,
           "SEED" : 7457769,
           "LR" : 1e-3/3,
           "BATCH_SIZE" : 128,
-          "DS_VERSION" : "v4",
+          "DS_VERSION" : "v5",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
           "DATA_FRACTION": 1.,
           "loss_criterion": torch.nn.MSELoss(),
