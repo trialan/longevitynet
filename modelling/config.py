db281f8073571363b6558656c40ffd620b0020e3
Thomas Rialan <thomasrialan@gmail.com>
2023-10-29 01:12:05 +0200

    bla

--- modelling/config.py
+++ modelling/config.py
@@ -13,14 +13,14 @@ REPO_DIR = get_repo_dir()
 
 CONFIG = {"N_EPOCHS" : 25,
           "SEED" : 7457769,
-          "LR" : 1e-3/3,
+          "LR" : 1e-3,
           "BATCH_SIZE" : 300, #128 uses 18GB, i have 46GB
           "DS_VERSION" : "v4",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
           "DATA_FRACTION": 1.,
           "loss_criterion": torch.nn.L1Loss(),
           "TARGET_SCALER": whiten,
-          "MODEL_CLASS": ResNet50,
+          "MODEL_CLASS": ViTCustom,
           "WEIGHT_DECAY": 5*10e-4}
 
 
