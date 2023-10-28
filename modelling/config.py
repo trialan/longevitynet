81ec8d3c67032190bffa8e9c84e9cbd0e4f53daf
Thomas Rialan <thomasrialan@gmail.com>
2023-10-29 00:57:29 +0200

    wd

--- modelling/config.py
+++ modelling/config.py
@@ -14,7 +14,7 @@ REPO_DIR = get_repo_dir()
 CONFIG = {"N_EPOCHS" : 25,
           "SEED" : 7457769,
           "LR" : 1e-3/3,
-          "BATCH_SIZE" : 128,
+          "BATCH_SIZE" : 300, #128 uses 18GB, i have 46GB
           "DS_VERSION" : "v4",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
           "DATA_FRACTION": 1.,
