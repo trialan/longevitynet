6f125687aa9c7e920a648045116d01501cc17907
Thomas Rialan <thomasrialan@gmail.com>
2023-10-29 00:19:25 +0200

    config

--- modelling/config.py
+++ modelling/config.py
@@ -18,9 +18,9 @@ CONFIG = {"N_EPOCHS" : 25,
           "DS_VERSION" : "v4",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
           "DATA_FRACTION": 1.,
-          "loss_criterion": torch.nn.L1Loss(),
+          "loss_criterion": torch.nn.MSELoss(),
           "TARGET_SCALER": min_max_scale,
-          "MODEL_CLASS": EfficientNetCustom}
+          "MODEL_CLASS": ViTCustom}
 
 
 print("=========TRAINING CONFIG=========")
