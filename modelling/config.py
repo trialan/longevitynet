5281a6aed224270407f96ef0e1bbd2400dafb305
Thomas Rialan <thomasrialan@gmail.com>
2023-10-29 01:30:22 +0200

    bla

--- modelling/config.py
+++ modelling/config.py
@@ -15,13 +15,13 @@ CONFIG = {"N_EPOCHS" : 25,
           "SEED" : 7457769,
           "LR" : 1e-3,
           "BATCH_SIZE" : 300, #128 uses 18GB, i have 46GB
-          "DS_VERSION" : "v5",
+          "DS_VERSION" : "v4",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
           "DATA_FRACTION": 1.,
           "loss_criterion": torch.nn.L1Loss(),
           "TARGET_SCALER": whiten,
           "MODEL_CLASS": ResNet50,
-          "WEIGHT_DECAY": 5*10e-4}
+          "WEIGHT_DECAY": 5*10e-3}
 
 
 print("=========TRAINING CONFIG=========")
