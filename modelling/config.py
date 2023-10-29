faee77b382710f9b63cb087cacba25c53fc42196
Thomas Rialan <thomasrialan@gmail.com>
2023-10-29 02:32:59 +0200

    bla

--- modelling/config.py
+++ modelling/config.py
@@ -17,10 +17,10 @@ CONFIG = {"N_EPOCHS" : 25,
           "BATCH_SIZE" : 300, #128 uses 18GB, i have 46GB
           "DS_VERSION" : "v4",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
-          "DATA_FRACTION": 0.5,
+          "DATA_FRACTION": 1.0,
           "loss_criterion": torch.nn.MSELoss(),
           "TARGET_SCALER": min_max_scale,
-          "MODEL_CLASS": ResNet50,
+          "MODEL_CLASS": EfficientNetCustom,
           "WEIGHT_DECAY": 0.}
 
 
