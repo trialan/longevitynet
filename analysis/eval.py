43ce500971600c666e0dfbc730eac233225e631e
Thomas Rialan <thomasrialan@gmail.com>
2023-10-30 19:10:53 +0100

    SE in eval

--- analysis/eval.py
+++ analysis/eval.py
@@ -18,17 +18,17 @@ def print_validation_stats_table(model, dataset_dict, benchmarks=True):
     return stats["loss"]
 
 
+
 def get_model_stats(model, dataset_dict):
     val_dataloader = dataset_dict['dataloaders']['validation']
     val_dataset = dataset_dict['datasets']['validation']
 
     predictions = compute_predictions(model, val_dataloader)
     loss = compute_batchwise_loss(predictions, val_dataloader)
-    mae = compute_batchwise_mae(predictions, val_dataloader)
+    mae, mae_standard_error = compute_batchwise_mae(predictions, val_dataloader)
     mae_in_years = convert_mae_to_years(mae, val_dataset)
 
-    return {"mae": mae_in_years, "loss": loss}
-
+    return {"mae": mae_in_years, "mae_standard_error": mae_standard_error, "loss": loss}
 
 def compute_batchwise_loss(predictions, val_dataloader):
     cumulative_loss = 0.0
@@ -43,17 +43,19 @@ def compute_batchwise_loss(predictions, val_dataloader):
 
 def compute_batchwise_mae(predictions, val_dataloader):
     cumulative_error = 0.0
+    errors = []
     for i, data in enumerate(val_dataloader):
         batch_targets = data['target']
         batch_predictions = torch.tensor(
             np.array(predictions[i * CONFIG["BATCH_SIZE"] : (i + 1) * CONFIG["BATCH_SIZE"]]),
             dtype=torch.float32,
         ).to(DEVICE)
-        batch_error = (
-            torch.abs(batch_predictions - batch_targets.to(DEVICE)).mean().item()
-        )
-        cumulative_error += batch_error
-    return cumulative_error / len(val_dataloader)
+        batch_errors = torch.abs(batch_predictions - batch_targets.to(DEVICE))
+        errors.extend(batch_errors.cpu().numpy())
+        cumulative_error += batch_errors.mean().item()
+    mae = cumulative_error / len(val_dataloader)
+    standard_error = np.std(errors) / np.sqrt(len(errors))
+    return mae, standard_error
 
 
 def compute_predictions(model, dataloader):
@@ -82,9 +84,9 @@ def get_mean_guess_stats(dataset_dict):
 
 def create_simple_results_table(rows):
     table = PrettyTable()
-    table.field_names = ["Method", "Loss", "Mean Abs. Error / years"]
+    table.field_names = ["Method", "Loss", "Mean Abs. Error / years", "SE"]
     for row in rows:
-        table.add_row(["Model", f"{row['loss']:.6f}", f"{row['mae']:.2f}"])
+        table.add_row(["Model", f"{row['loss']:.6f}", f"{row['mae']:.2f}", f"{row['mae_standard_error']:.2f}"])
     return table
 
 
