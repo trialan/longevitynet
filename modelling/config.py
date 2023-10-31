7f540d58866181480fe2e221c0668dc7da3a0439
Thomas Rialan <thomasrialan@gmail.com>
2023-10-31 10:52:16 +0100

    conf tweak

--- modelling/config.py
+++ modelling/config.py
@@ -14,14 +14,14 @@ REPO_DIR = get_repo_dir()
 CONFIG = {"N_EPOCHS" : 25,
           "SEED" : 7457769,
           "LR" : 1e-3 / 3.,
-          "BATCH_SIZE" : 128, #128 uses 18GB, i have 46GB
+          "BATCH_SIZE" : 300, #128 uses 18GB, i have 46GB
           "DS_VERSION" : "v5",
           "MODEL_DEVICE": "cuda" if torch.cuda.is_available() else "mps",
           "DATA_FRACTION": 1.0,
           "loss_criterion": torch.nn.L1Loss(),
           "TARGET_SCALER": min_max_scale,
           "MODEL_CLASS": ResNet50,
-          "WEIGHT_DECAY": 0.}
+          "WEIGHT_DECAY": 0.05}
 
 
 print("=========TRAINING CONFIG=========")
