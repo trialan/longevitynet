5ddd78fcb6172b2ebc43c114707ba351cdc61265
Thomas Rialan <thomasrialan@gmail.com>
2023-10-31 11:35:22 +0100

    vgg run

--- modelling/config.py
+++ modelling/config.py
@@ -20,7 +20,7 @@ CONFIG = {"N_EPOCHS" : 25,
           "DATA_FRACTION": 1.0,
           "loss_criterion": torch.nn.L1Loss(),
           "TARGET_SCALER": min_max_scale,
-          "MODEL_CLASS": ViTCustom,
+          "MODEL_CLASS": VGG16, 
           "WEIGHT_DECAY": 0.0}
 
 
