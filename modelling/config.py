d8cb0b299ae3bddf251450764aa8a9c8b2905239
Thomas Rialan <thomasrialan@gmail.com>
2023-10-31 11:38:11 +0100

    efficientnet run

--- modelling/config.py
+++ modelling/config.py
@@ -20,7 +20,7 @@ CONFIG = {"N_EPOCHS" : 25,
           "DATA_FRACTION": 1.0,
           "loss_criterion": torch.nn.L1Loss(),
           "TARGET_SCALER": min_max_scale,
-          "MODEL_CLASS": VGG16, 
+          "MODEL_CLASS": EfficientNetCustom,
           "WEIGHT_DECAY": 0.0}
 
 
